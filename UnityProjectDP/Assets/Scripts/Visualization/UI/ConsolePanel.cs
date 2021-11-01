a89e57e2e124f9def260560ea5bcbb105d44dd13
Filip-Novak <51423248+Filip-Novak@users.noreply.github.com>
2021-11-01 12:25:11 +0100

    Input/output fields with Lukas

---
+++ UnityProjectDP/Assets/Scripts/Visualization/UI/ConsolePanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ConsolePanel : MonoBehaviour
+{
+    public GameObject inputField;
+    public GameObject outputField;
+
+    public void YieldOutput(string output) 
+    {
+        TMP_InputField tmpInpField =  outputField.GetComponent<TMP_InputField>();
+        tmpInpField.text += output + "\n";
+    }
+}
