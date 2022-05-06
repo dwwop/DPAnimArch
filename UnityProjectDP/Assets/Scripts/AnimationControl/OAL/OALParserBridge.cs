83a3774602944d413bc657250e1372a0aa63309a
Filip-Novak <51423248+Filip-Novak@users.noreply.github.com>
2022-05-06 17:56:58 +0200

    Cleared code

--- UnityProjectDP/Assets/Scripts/AnimationControl/OAL/OALParserBridge.cs
+++ UnityProjectDP/Assets/Scripts/AnimationControl/OAL/OALParserBridge.cs
@@ -57,30 +57,23 @@ namespace Assets.Scripts.AnimationControl.OAL
                 ITokenSource lexer = new OALLexer(target);
                 ITokenStream tokens = new CommonTokenStream(lexer);
                 parser = new OALParser(tokens);
-                parser.BuildParseTree = true;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
-
             }
 
-            //ExprParser.LiteralContext result = parser.literal();
             OALParser.LinesContext result = parser.lines();
-            Console.Write(result.ToStringTree());
-            Console.WriteLine();
-
-            OALToPythonVisitor test = new OALToPythonVisitor(Attributes);
+            OALToPythonVisitor visitor = new OALToPythonVisitor(Attributes);
             String pythonCode = "";
 
             try
             {
-                pythonCode = test.VisitLines(result);
+                pythonCode = visitor.VisitLines(result);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
-
             }
 
             return pythonCode;
--- UnityProjectDP/Assets/Scripts/AnimationControl/OAL/OALToPythonVisitor.cs
+++ UnityProjectDP/Assets/Scripts/AnimationControl/OAL/OALToPythonVisitor.cs
@@ -15,7 +15,7 @@ namespace AnimationControl.OAL
         public OALToPythonVisitor(List<String> attributesList)
         {
             this.tab = "\t\t"; //mozno sa bude menit
-            this.attributesList = attributesList;
+            this.attributesList = attributesList; //mozno budeme potrebovat neskor
         }
 
         public override String VisitLines([NotNull] OALParser.LinesContext context)
@@ -28,16 +28,13 @@ namespace AnimationControl.OAL
                 lines += Visit(context.GetChild(i));
             }           
             
-            return lines;
-            //return base.VisitCode(context);  
+            return lines; 
         }
 
         public override String VisitLine([NotNull] OALParser.LineContext context)
         {
             String line = this.tab + VisitChildren(context) + Environment.NewLine;
             return line;
-            
-            //return base.VisitLine(context);
         }
 
         public override String VisitExeCommandQueryCreate([NotNull] OALParser.ExeCommandQueryCreateContext context)
@@ -47,11 +44,6 @@ namespace AnimationControl.OAL
             {
                 String instanceName = Visit(context.GetChild(1));
 
-                /*if (this.attributesList.Contains(instanceName))
-                {
-                    instanceName = "self." + instanceName;
-                }*/
-
                 return instanceName + " = " + context.GetChild(3).GetText() + "()";
             }
             else if (context.GetChild(1).GetType().Name.Equals("KeyLetterContext"))
@@ -60,7 +52,6 @@ namespace AnimationControl.OAL
             }
 
             return null;
-            //return base.VisitQueryCreateClass(context);
         }
 
         public override String VisitExeCommandQueryRelate([NotNull] OALParser.ExeCommandQueryRelateContext context)
@@ -72,11 +63,6 @@ namespace AnimationControl.OAL
         {        
             String instanceName = Visit(context.GetChild(1));
             String keyLetter = context.GetChild(3).GetText();
-
-            /*if (this.attributesList.Contains(instanceName))
-            {
-                instanceName = "self." + instanceName;
-            }*/
 
             if (context.GetChild(0).GetText().Contains("many"))
             {
@@ -104,7 +90,6 @@ namespace AnimationControl.OAL
             }
             
             return null;
-            //return base.VisitQuerySelectClass(context);
         }
 
         public override String VisitExeCommandQuerySelectRelatedBy([NotNull] OALParser.ExeCommandQuerySelectRelatedByContext context)
@@ -116,13 +101,7 @@ namespace AnimationControl.OAL
         {   //problem je s delete alebo "select any" pretoze nevieme pozistovat vsetky referencie na odstranenie
             String instanceName = Visit(context.GetChild(1));
 
-            /*if (this.attributesList.Contains(instanceName))
-            {
-                instanceName = "self." + instanceName;
-            }*/
-
             return "del " + instanceName;
-            //return base.VisitQueryDeleteClass(context);
         }
 
         public override String VisitExeCommandQueryUnrelate([NotNull] OALParser.ExeCommandQueryUnrelateContext context)
@@ -131,7 +110,7 @@ namespace AnimationControl.OAL
         }
 
         public override String VisitExeCommandAssignment([NotNull] OALParser.ExeCommandAssignmentContext context)
-        {   //expr nemusime pozerat ci je attribute lebo to robime v expression commande
+        {   // nemusime pozerat ci je attribute lebo to robime v expression commande
             String expr;
             String instanceName;
 
@@ -146,102 +125,64 @@ namespace AnimationControl.OAL
                 instanceName = Visit(context.GetChild(0));
             }
 
-            /*if (this.attributesList.Contains(instanceName))
-            {
-                instanceName = "self." + instanceName;
-            }*/
-
             return instanceName + " = " + expr;
-            //return base.VisitVariableAssignment(context);
         }
 
         public override String VisitExeCommandCall([NotNull] OALParser.ExeCommandCallContext context)
         {
-            /*if (context.GetChild(1).GetText().Equals("."))
-            {*/
-                string instanceName = Visit(context.GetChild(0));
+            string instanceName = Visit(context.GetChild(0));
 
-                /*if (this.attributesList.Contains(instanceName))
-                {
-                    instanceName = "self." + instanceName;
-                }*/
-
-                if (context.GetChild(3).GetText().Equals("(") && context.GetChild(4).GetText().Equals(")"))
-                {
-                    return instanceName + "." + context.GetChild(2).GetText() + "()";
-                }
-                else
-                {
-                    String parameters = "";
-
-                    for (int i = 4; i < context.ChildCount; i++)
-                    {
-                        if (context.GetChild(i).GetType().Name.Equals("ExprContext"))
-                        {
-                            parameters += Visit(context.GetChild(i));
-                        }
-                        else if (context.GetChild(i).GetText().Equals(","))
-                        {
-                            parameters += ", ";
-                        }
-                        else if (context.GetChild(i).GetText().Equals(")"))
-                        {
-                            break;
-                        }
-                    }
-
-                    return instanceName + "." + context.GetChild(2).GetText() + "(" + parameters + ")";
-                }     
-            /*}
+            if (context.GetChild(3).GetText().Equals("(") && context.GetChild(4).GetText().Equals(")"))
+            {
+                return instanceName + "." + context.GetChild(2).GetText() + "()";
+            }
             else
             {
-                return context.GetChild(7).GetText() + "()." + context.GetChild(9).GetText() + "()";
-            }*/
-            
-            //return base.VisitCommandCall(context);
+                String parameters = "";
+
+                for (int i = 4; i < context.ChildCount; i++)
+                {
+                    if (context.GetChild(i).GetType().Name.Equals("ExprContext"))
+                    {
+                        parameters += Visit(context.GetChild(i));
+                    }
+                    else if (context.GetChild(i).GetText().Equals(","))
+                    {
+                        parameters += ", ";
+                    }
+                    else if (context.GetChild(i).GetText().Equals(")"))
+                    {
+                        break;
+                    }
+                }
+
+                return instanceName + "." + context.GetChild(2).GetText() + "(" + parameters + ")";
+            }
         }
 
         public override String VisitExeCommandCreateList([NotNull] OALParser.ExeCommandCreateListContext context)
         {
             String listName = Visit(context.GetChild(1));
-
-            /*if (this.attributesList.Contains(listName))
-            {
-                listName = "self." + listName;
-            }*/
             
             if (context.GetChild(4).GetText().Equals("{"))
             {
                 String items = "";
                 for (int i = 5; i < context.ChildCount - 2; i++)
                 {
-                    if (context.GetChild(i).GetText().Equals(","))      //pre krajsi kod davame medzeri
+                    if (context.GetChild(i).GetText().Equals(","))
                     {
                         items += ", ";
                     }
                     else if (context.GetChild(i).GetType().Name.Equals("InstanceHandleContext"))
                     {
-                        /*if (this.attributesList.Contains(context.GetChild(i).GetText()))
-                        {
-                            items += "self." + context.GetChild(i).GetText();
-                        }
-                        else
-                        {
-                            items += context.GetChild(i).GetText();
-                        }*/
                         items += Visit(context.GetChild(i));
                     }
-                    /*else
-                    {
-                        items += context.GetChild(i).GetText();
-                    }*/  
                 }
 
                 return listName + " = [" + items + "]";
             }
 
             return listName + " = []";
-            //return base.VisitQueryCreateList(context);
         }
 
         public override String VisitExeCommandAddingToList([NotNull] OALParser.ExeCommandAddingToListContext context) 
@@ -249,17 +190,7 @@ namespace AnimationControl.OAL
             String instanceName = Visit(context.GetChild(1));
             String listName = Visit(context.GetChild(3));
 
-            /*if (this.attributesList.Contains(instanceName))
-            {
-                instanceName = "self." + instanceName;
-            }
-            if (this.attributesList.Contains(listName))
-            {
-                listName = "self." + listName;
-            }*/
-
             return listName + ".append(" + instanceName + ")";
-            //return base.VisitAddingToList(context);
         }
 
         public override String VisitExeCommandRemovingFromList([NotNull] OALParser.ExeCommandRemovingFromListContext context)
@@ -267,18 +198,7 @@ namespace AnimationControl.OAL
             String instanceName = Visit(context.GetChild(1));
             String listName = Visit(context.GetChild(3));
 
-            /*if (this.attributesList.Contains(instanceName))
-            {
-                instanceName = "self." + instanceName;
-            }
-            if (this.attributesList.Contains(listName))
-            {
-                listName = "self." + listName;
-            }*/
-
             return listName + " = [instance for instance in " + listName + " if instance != " + instanceName + "]";
-            //return base.VisitRemovingFromList(context);
-
         }
 
         public override String VisitExeCommandWrite([NotNull] OALParser.ExeCommandWriteContext context)
@@ -289,7 +209,7 @@ namespace AnimationControl.OAL
 
                 for (int i = 2; i < context.ChildCount - 2; i++)
                 {
-                    if (context.GetChild(i).GetText().Equals(","))      //pre krajsi kod davame medzeri
+                    if (context.GetChild(i).GetText().Equals(","))
                     {
                         arguments += ", ";
                     }
@@ -303,7 +223,6 @@ namespace AnimationControl.OAL
             }
 
             return "print()";
-            //return base.VisitCommandWrite(context);
         }
 
         public override String VisitExeCommandRead([NotNull] OALParser.ExeCommandReadContext context)
@@ -314,11 +233,6 @@ namespace AnimationControl.OAL
             if (context.GetChild(0).GetText().Equals("assign "))
             {
                 variableName = Visit(context.GetChild(1));
-
-                /*if (this.attributesList.Contains(variableName))
-                {
-                    variableName = "self." + variableName;
-                }*/
 
                 if (context.GetChild(4).GetType().Name.Equals("ExprContext"))
                 {
@@ -346,11 +260,6 @@ namespace AnimationControl.OAL
             {
                 variableName = Visit(context.GetChild(0));
 
-                /*if (this.attributesList.Contains(variableName))
-                {
-                    variableName = "self." + variableName;
-                }*/
-
                 if (context.GetChild(3).GetType().Name.Equals("ExprContext"))
                 {
                     prompt = Visit(context.GetChild(3));
@@ -373,8 +282,6 @@ namespace AnimationControl.OAL
                     return variableName + " = input(" + prompt + ")";
                 }
             }
-
-            //return base.VisitCommandRead(context);
         }
 
         public override String VisitIfCommand([NotNull] OALParser.IfCommandContext context)
@@ -410,7 +317,6 @@ namespace AnimationControl.OAL
             this.tab = temp_tab;
             //return lines;
             return "if " + expr + ":" + Environment.NewLine + lines;
-            //return base.VisitIfStatement(context);
         }
 
         public override String VisitWhileCommand([NotNull] OALParser.WhileCommandContext context)
@@ -434,7 +340,6 @@ namespace AnimationControl.OAL
 
             this.tab = temp_tab;
             return "while (" + expr + "):" + Environment.NewLine + lines;   
-            //return base.VisitWhileStatement(context);
         }
 
         public override String VisitForeachCommand([NotNull] OALParser.ForeachCommandContext context)
@@ -457,13 +362,7 @@ namespace AnimationControl.OAL
 
             this.tab = temp_tab;
 
-            /*if (this.attributesList.Contains(context.GetChild(3).GetText()))
-            {
-                return "for " + context.GetChild(1).GetText() + " in self." + context.GetChild(3).GetText() + ":" + Environment.NewLine + lines;
-            }*/
-
             return "for " + context.GetChild(1).GetText() + " in " + Visit(context.GetChild(3)) + ":" + Environment.NewLine + lines;
-            //return base.VisitForeachStatement(context);
         }
 
         public override String VisitReturnCommand([NotNull] OALParser.ReturnCommandContext context)
@@ -476,19 +375,16 @@ namespace AnimationControl.OAL
             }
 
             return "return";
-            //return base.VisitReturnCommand(context);
         }
 
         public override String VisitContinueCommand([NotNull] OALParser.ContinueCommandContext context)
         {
             return "continue";
-            //return base.VisitContinueCommand(context);
         }
 
         public override String VisitBreakCommand([NotNull] OALParser.BreakCommandContext context)
         {
             return "break";
-            //return base.VisitBreakCommand(context);
         }
 
         public override String VisitParCommand([NotNull] OALParser.ParCommandContext context)
@@ -499,14 +395,13 @@ namespace AnimationControl.OAL
         public override String VisitCommentCommand([NotNull] OALParser.CommentCommandContext context)
         {
             return context.GetText();
-            //return base.VisitComment(context);
         }
 
         public override String VisitInstanceHandle([NotNull] OALParser.InstanceHandleContext context)
         {
             String instanceName = context.GetChild(0).GetText();
 
-            /*if (this.attributesList.Contains(instanceName)) //mozno budeme potrebovat
+            /*if (this.attributesList.Contains(instanceName)) //mozno budeme potrebovat neskor
             {
                 instanceName = "self." + instanceName;
             }*/
@@ -519,7 +414,6 @@ namespace AnimationControl.OAL
             {
                 return instanceName + "." + context.GetChild(2).GetText();
             }
-            //return base.VisitInstanceHandle(context);
         }
 
         public override String VisitExpr([NotNull] OALParser.ExprContext context)
@@ -543,7 +437,7 @@ namespace AnimationControl.OAL
                 }
                 else
                 {
-                    /*if (this.attributesList.Contains(context.GetChild(0).GetText()))  //mozno budeme potrebovat
+                    /*if (this.attributesList.Contains(context.GetChild(0).GetText()))  //mozno budeme potrebovat neskor
                     {
                         return "self." + context.GetChild(0).GetText();
                     }*/
@@ -565,31 +459,14 @@ namespace AnimationControl.OAL
                 }
                 else if (context.GetChild(0).GetText().Equals("cardinality "))
                 {
-                    /*string instanceName = context.GetChild(1).GetText();
-
-                    if (this.attributesList.Contains(instanceName))
-                    {
-                        instanceName = "self." + instanceName;
-                    }*/
-
                     return "cardinality(" + Visit(context.GetChild(1)) + ")";
                 }
                 else if (context.GetChild(0).GetText().Equals("empty "))
                 {
-                    /*if (this.attributesList.Contains(context.GetChild(1).GetText()))
-                    {
-                        return "not self." + context.GetChild(1).GetText();
-                    }*/
-
                     return "not " + Visit(context.GetChild(1));
                 }
                 else if (context.GetChild(0).GetText().Equals("not_empty "))
                 {
-                    /*if (this.attributesList.Contains(context.GetChild(1).GetText()))
-                    {
-                        return "self." + context.GetChild(1).GetText();
-                    }*/
-
                     return Visit(context.GetChild(1));
                 }
             }
@@ -608,7 +485,7 @@ namespace AnimationControl.OAL
                 }
                 else
                 {
-                    /*if (this.attributesList.Contains(context.GetChild(0).GetText()))  //mozno budeme potrebovat
+                    /*if (this.attributesList.Contains(context.GetChild(0).GetText()))  //mozno budeme potrebovat neskor
                     {
                         return "self." + context.GetChild(0).GetText() + "." + context.GetChild(2).GetText();
                     }*/
@@ -618,7 +495,6 @@ namespace AnimationControl.OAL
             }
 
             return null;
-            //return base.VisitExpr(context);
         }
     }
 }
