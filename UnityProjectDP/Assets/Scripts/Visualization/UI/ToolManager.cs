013b957150e5e92cf8903dd3460ae1436598775d
olga_charna <olya13771@gmail.com>
2022-09-07 10:40:38 +0200

    teleport medzi vrstvami

--- UnityProjectDP/Assets/Scripts/Visualization/UI/ToolManager.cs
+++ UnityProjectDP/Assets/Scripts/Visualization/UI/ToolManager.cs
@@ -10,11 +10,13 @@ namespace AnimArch.Visualization.UI
         public string SelectedTool { set; get; }
         public bool ZoomingIn { set; get; } = false;
         public bool ZoomingOut { set; get; } = false;
+        public bool IsJump { set; get; } = false;
+        public int InterGraphJump { set; get; } = 0;
         public Color SelectedColor { set; get; }
         public bool isAnimating = false;
         public bool overUi = false;
-        [SerializeField]
-        private string startingSelectedColor;
+        [SerializeField] private string startingSelectedColor;
+
         public void SelectTool(string toolName)
         {
             SelectedTool = toolName;
@@ -27,29 +29,40 @@ namespace AnimArch.Visualization.UI
                 MenuManager.Instance.ActivatePanelColors(false);
             }
         }
+
         public void ZoomIn(bool enabled)
         {
             ZoomingIn = enabled;
         }
+
+        public void Jump()
+        {
+            IsJump = true;
+            InterGraphJump = 1 - InterGraphJump;
+        }
+
         public void Start()
         {
             SelectColor(startingSelectedColor);
         }
+
         public void ZoomOut(bool enabled)
         {
             ZoomingOut = enabled;
         }
+
         public void SelectColor(string colorID)
         {
             Color c;
             if (colorID.Contains("#"))
             {
-                ColorUtility.TryParseHtmlString( colorID, out c);
+                ColorUtility.TryParseHtmlString(colorID, out c);
             }
             else
             {
                 ColorUtility.TryParseHtmlString("#" + colorID + "ff", out c);
             }
+
             SelectedColor = c;
         }
     }
