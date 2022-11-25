98582a33fd82e8af99d7389420aab6c161b40fc6
Martin <martin.vanko96@gmail.com>
2022-11-25 20:23:07 +0100

    player related scripts, prefab, introduction scene

---
+++ UnityProjectDP/Assets/Scripts/Visualization/Networking/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Networking
+{
+    class Player : NetworkBehaviour
+    {
+        public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+
+        public string Name;
+
+        [SerializeField]
+        public Color Color;
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        public override void OnNetworkSpawn()
+        {
+            if (IsServer)
+            {
+                Color = Color.blue;
+            }
+            if (IsClient)
+            {
+                Color = Color.green;
+            }
+        }
+    }
+}
---
+++ UnityProjectDP/Assets/Scripts/Visualization/Networking/PlayerManager.cs
@@ -0,0 +1,77 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace Networking
+{
+    public class PlayerManager : NetworkSingleton<PlayerManager>
+    {
+        static string playerName = "Enter name";
+
+        private void Start()
+        {
+            NetworkManager.Singleton.OnClientConnectedCallback += (id) =>
+            {
+                var playerObject = NetworkManager.LocalClient.PlayerObject;
+                var no = playerObject.GetComponent<NetworkObject>();
+                if (id == 0)
+                {
+                    var player = playerObject.GetComponent<Player>();
+                    player.Color = Color.green;
+                }
+                else
+                {
+                    var player = playerObject.GetComponent<Player>();
+                    player.Color = Color.cyan;
+                }
+            };
+        }
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            {
+                playerName = GUILayout.TextField(playerName, 25);
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+            }
+
+            GUILayout.EndArea();
+        }
+
+        static void StartButtons()
+        {
+            if (GUILayout.Button("Host"))
+            {
+                NetworkManager.Singleton.StartHost();
+                SceneManager.LoadScene("AnimArch");
+            }
+            if (GUILayout.Button("Client"))
+            {
+                NetworkManager.Singleton.StartClient();
+            }
+
+            if (GUILayout.Button("Server"))
+            {
+                NetworkManager.Singleton.StartServer();
+            }
+        }
+
+        static void StatusLabels()
+        {
+            var mode = NetworkManager.Singleton.IsHost ?
+                "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+            GUILayout.Label("Transport: " +
+                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+            GUILayout.Label("Mode: " + mode);
+        }
+    }
+}
